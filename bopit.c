49b99f7ca51be847a5f2c9fd1802824735971d4c
Jonathan Galaydick <jtgalay@comcast.net>
2018-11-06 21:22:17 -0500

    I think everything's done except for pinouts and the sounds

--- bopit.c
+++ bopit.c
@@ -1,9 +1,22 @@
 //BOPIT IS FUN
 
-//reference: https://gist.github.com/c-eastman/5380163
+//reference for general gameplay: https://gist.github.com/c-eastman/5380163
+//reference for sounds: https://www.arduino.cc/en/Tutorial/toneMelody
+//reference for timer: hopefully this https://learn.adafruit.com/multi-tasking-the-arduino-part-1/using-millis-for-timing
 
 
 #include <LiquidCrystal.h>
+
+//example tone definitions, have to finalize this eventually
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
 
 //set all LEDs and buttons to needed inputs
 //numbers are obviously not correct as of now
@@ -13,20 +26,22 @@ const int userSlider = 6;
 const int userToggle = 7;
 //does the LCD screen need something?
 
-//store the state of the inputs
-int stateButtonState = 0;
-int userButtonState = 0;
-int userSliderState = 0;
-int userToggleState = 0;
+//store the state of the inputs (might not need this)
+// int stateButtonState = 0;
+// int userButtonState = 0;
+// int userSliderState = 0;
+// int userToggleState = 0;
 
 boolean gameOn = false;
 
 int userChoice = 0;
 int score = 0;
 int turn = 0; //turn number, used to index array of commands
-double waitTime = 600; //max time between command given and user action
+long delayTime = 5000; //wait time for user to make an action
 
-int commands[30];
+int commands[99]; //game commands
+
+unsigned long previousMills = 0;
 
 
 
@@ -44,47 +59,107 @@ void setup() {
 
 void loop() {
 	
+	// stateButtonState = digitalRead(startButton);
+	// userButtonState = digitalRead(userButton);
+	// userSliderState = digitalRead(userSlider);
+	// userToggleState = digitalRead(userToggle);
+	
 	//when the game button is pressed setup 
 	if (startButtonState == 1) { //how to represent high? 1?
-		//reset turn count and score
-		score = 0;
-		turn = 0;
-		gameOn = true;
-		
-		for (int i = 0; i < 100; i++){ //used to generate random commands 
-			commands[i] = random(0,3); //random commands are 0, 1, 2
-		}
+		resetGame();
 	}
 	
-	stateButtonState = digitalRead(startButton);
-	userButtonState = digitalRead(userButton);
-	userSliderState = digitalRead(userSlider);
-	userToggleState = digitalRead(userToggle);
+	unsigned long currentMills = millis();
+
 	
 	//the main loop for playing the game
 	if (gameOn) {
 		
+		//this part only gets done once per player turn
+		//need to think of a way to make it happen only once
+		if (command[score] == 1) {
+			//give button command sound
+			LCD_update("BUTTON");
+			playSound(NOTE_A3)
+		} else if (command[score] == 2) {
+			//give slider command sound
+			LCD_update("SLIDE");
+			playSound(NOTE_C4)
+		} else if (command[score] == 3) {
+			//give toggle command sound
+			LCD_update("TOGGLE");
+			playSound(NOTE_G1)
+		} else {
+			LCD_update("COMMAND ISSUE");
+		}
+		
+		if (currentMills - previousMills > delayTime) {
+			
+			if (digitalRead(userButton) == HIGH) {
+				userChoice = 1;
+			} else if (digitalRead(userSlider) == HIGH) {
+				userChoice = 2;
+			} else if (digitalRead(userToggle) == HIGH) {
+				userChoice = 3;
+			}
+			
+			if (userChoice == command[score]) {
+				//correct action made in correct amount of time
+				score += 1;
+				LCD_update("Correct!");
+				delayTime -= 40;
+				previousMills = currentMills; //reset countdown for next turn
+			} else if (userChoice != 0 && userChoice != command[turn]) {
+				//incorrect action made in the time frame
+				LCD_update("Game Over!");
+				gameOn = false;
+			}
+			
+		} else {
+			LCD_update("Game Over!")
+			gameOn = false;
+		}
+			
 	}
 	
 }
 
-
-/* //function for comparing user input with desired input
-int check (choice, desired) {
-	if (desired == choice) {
-		//blink green LED
-		score += 1;
-		return 1;
+//called when game start button is pressed
+void resetGame() {
+		score = 0;
+		turn = 0;
+		gameOn = true;
+		previousMills = millis(); //set starting time of the game
 		
-	} else {
-		//blink red led
-		//display score
-		//end game
-		return -1;
-	}
-} */
+		for (int i = 0; i < 100; i++){ //used to generate random commands 
+			commands[i] = random(1, 4); //random commands are 1, 2, 3
+			//0 maps to the button
+			//1 maps to the sldier
+			//2 maps to the toggle
+		}
+}
 
-//updates the LCD screen with a text indicator of the current score
-void LCD update(String text, int color {
-	LCD.print(text);
+//updates the LCD screen with game state and current score
+void LCD_update(String text) {
+	lcd.clear();
+	lcd.setCursor(3,0);
+	lcd.print(text);
+	
+	//convert score to a string variant, then print it on the LCD on a new line
+	lcd.setCursor(3,0);
+	char scoreString[12];
+	sprintf(scoreString, "Score: %d", score);
+	lcd.print(scoreString);
 )
+
+//takes the int representation of a sound and plays it to the speaker
+void playSound(int sound) {
+	
+}
+
+
+
+
+
+
+
